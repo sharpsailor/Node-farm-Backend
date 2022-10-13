46e2d881b1b2c097641d982746f485754b5c3ce0
sharpsailor <sharpsailor13@gmail.com>
2022-10-13 06:53:26 +0530

    imported modules template created

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const fs = require("fs");
-const http = require("http");
-const url = require("url");
-const replaceTemplate=require("./modules/replaceTemplate");
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+const slugify = require('slugify');
+const replaceTemplate = require('./modules/replaceTemplate');
 //////////////////////////////////////////
 // Files
 // Calls in NODEJS
@@ -38,52 +39,56 @@ console.log("Will Read file ");
 
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
-  "utf-8"
+  'utf-8'
 );
 const tempCard = fs.readFileSync(
   `${__dirname}/templates/template-card.html`,
-  "utf-8"
+  'utf-8'
 );
 const tempProduct = fs.readFileSync(
   `${__dirname}/templates/template-product.html`,
-  "utf-8"
+  'utf-8'
 );
 
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
+// console.log(slugify("Fresh Avocados", { lower: true }));
+console.log(slugs);
 const server = http.createServer((req, res) => {
   // console.log(req.url);
   const { query, pathname } = url.parse(req.url, true);
+
   // OVERVIEW PAGE
-  if (pathname === "/" || pathname === "/overview") {
-    res.writeHead(200, { "Content-type": "text/html" });
+  if (pathname === '/' || pathname === '/overview') {
+    res.writeHead(200, { 'Content-type': 'text/html' });
     const cardsHtml = dataObj
       .map((el) => replaceTemplate(tempCard, el))
-      .join("");
-    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+      .join('');
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
     res.end(output);
     // PRODUCT PAGE
-  } else if (pathname === "/product") {
-    res.writeHead(200, { "Content-type": "text/html" });
+  } else if (pathname === '/product') {
+    res.writeHead(200, { 'Content-type': 'text/html' });
     const product = dataObj[query.id];
     const output = replaceTemplate(tempProduct, product);
     res.end(output);
     // res.end("This is Product");
     // API
-  } else if (pathname === "/api") {
-    res.writeHead(200, { "Content-type": "application/json" });
+  } else if (pathname === '/api') {
+    res.writeHead(200, { 'Content-type': 'application/json' });
     res.end(data);
     // NOT FOUND
   } else {
     res.writeHead(404, {
-      "Content-type": "text/html",
-      "my-Own-header": `hello- world`,
+      'Content-type': 'text/html',
+      'my-Own-header': `hello- world`,
     });
-    res.end("<h1>Page not found!</h1>");
+    res.end('<h1>Page not found!</h1>');
   }
   // res.end("Hello From the Sharpsailor server!");
 });
 
-server.listen(8000, "127.0.0.1", () => {
+server.listen(8000, '127.0.0.1', () => {
   console.log(`Listening to request on port 8000`);
 });
